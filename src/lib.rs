77ecfcb4f2cd5171667fb7b93d80a393fb0369f5
Simon Crowe <simon.r.crowe@protonmail.com>
2022-11-26 18:45:51 +0000

    Add logic for determing which nodes to draw within a rect

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use byteorder::{ByteOrder, LittleEndian};
 use js_sys::Reflect::get;
 use js_sys::{Object, Uint8Array};
-use std::collections::HashMap;
+use std::cmp;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
@@ -19,35 +19,59 @@ pub fn get_memory() -> JsValue {
 
 #[wasm_bindgen]
 pub struct Graph {
-    node_targets: HashMap<u16, Vec<u16>>,
-    node_locations: HashMap<u16, Vec<u32>>,
+    node_targets: Vec<Vec<usize>>,
+    node_locations: Vec<Vec<f64>>,
 }
 
 #[wasm_bindgen]
 impl Graph {
     pub fn new() -> Graph {
-        let node_targets = HashMap::new();
-        let node_locations = HashMap::new();
+        let node_targets = Vec::new();
+        let node_locations = Vec::new();
         Graph {
             node_targets,
             node_locations,
         }
     }
 
-    pub fn node_count(&mut self) -> usize {
-        self.node_targets.len()
+    pub fn node_count(&self) -> usize {
+        // TODO: delete if unused
+        cmp::min(self.node_targets.len(), self.node_locations.len())
     }
 
-    pub fn node_targets_count(&mut self, node_id: u16) -> usize {
-        self.node_targets.get(&node_id).unwrap().len()
+    pub fn node_targets_count(&self, node_id: usize) -> usize {
+        self.node_targets.get(node_id - 1).unwrap().len()
     }
 
-    pub fn node_targets_ptr(&mut self, node_id: u16) -> *const u16 {
-        self.node_targets.get(&node_id).unwrap().as_ptr()
+    pub fn node_targets_ptr(&self, node_id: usize) -> *const usize {
+        self.node_targets.get(node_id - 1).unwrap().as_ptr()
     }
 
-    pub fn node_location_ptr(&mut self, node_id: u16) -> *const u32 {
-        self.node_locations.get(&node_id).unwrap().as_ptr()
+    pub fn node_location_ptr(&self, node_id: usize) -> *const f64 {
+        self.node_locations.get(node_id - 1).unwrap().as_ptr()
+    }
+
+    pub fn node_ids_to_render(&self, rect: Rect) -> Vec<usize> {
+        let contained_indices = self
+            .node_locations
+            .iter()
+            .enumerate()
+            .filter(|(_idx, loc)| rect.contains(loc))
+            .map(|(idx, _loc)| idx);
+
+        let neighbouring_indices = contained_indices
+            .clone()
+            .map(|idx| self.neighbours(idx))
+            .flatten();
+
+        let mut result = contained_indices
+            .chain(neighbouring_indices)
+            .map(|idx| idx + 1)
+            .collect::<Vec<usize>>();
+
+        result.sort();
+        result.dedup();
+        result
     }
 
     pub async fn load_edges(&mut self) -> Result<(), JsValue> {
@@ -57,15 +81,19 @@ impl Graph {
         let resp: Response = resp_value.dyn_into().unwrap();
         log(&format!("Response status code: {}", resp.status()));
         if resp.status() != 200 {
-            return Err(JsValue::FALSE);
+            let msg = format!(
+                "Bad status code when getting targets binary: {}",
+                resp.status()
+            );
+            return Err(JsValue::from(msg));
         }
         let reader_value = resp.body().unwrap().get_reader();
         let reader: ReadableStreamDefaultReader = reader_value.dyn_into().unwrap();
 
-        let mut current_node_id: u16 = 1;
-        self.node_locations.insert(current_node_id, vec![0u32; 2]);
-        self.node_targets.insert(current_node_id, Vec::new());
-        log(&format!("Getting targets for node {}", current_node_id));
+        self.node_locations.push(random_location(1024.0));
+        self.node_targets.push(Vec::new());
+        let mut current_node_index: usize = 0;
+        log(&format!("Getting targets for node {}", current_node_index));
         loop {
             let result_value = JsFuture::from(reader.read()).await?;
             let result: Object = result_value.dyn_into().unwrap();
@@ -75,7 +103,7 @@ impl Graph {
             if done_value.as_bool().unwrap() {
                 log(&format!(
                     "Done. Loaded targets for {} nodes",
-                    current_node_id - 1
+                    current_node_index - 1
                 ));
                 break;
             }
@@ -89,22 +117,70 @@ impl Graph {
             log(&"Instantiated a new u16 vector!");
             LittleEndian::read_u16_into(&chunk_buffer, &mut numbers);
             log(&"Filled u16 vector from the buffer!");
-            log(&format!("Getting targets for node {}...", current_node_id));
+            log(&format!(
+                "Getting targets for node {}...",
+                current_node_index
+            ));
             for &num in numbers.iter() {
+                // The MAX acts as a delimiter
                 if num == u16::MAX {
-                    // The MAX acts as a delimiter
-                    current_node_id += 1;
-                    self.node_locations.insert(current_node_id, vec![0; 2]);
-                    self.node_targets.insert(current_node_id, Vec::new());
+                    self.node_locations.push(random_location(1024.0));
+                    self.node_targets.push(Vec::new());
+                    current_node_index += 1;
                 } else {
                     self.node_targets
-                        .get_mut(&current_node_id)
+                        .get_mut(current_node_index)
                         .unwrap()
-                        .push(num);
+                        .push(num as usize);
                 }
             }
         }
 
         Ok(())
+    }
+}
+
+impl Graph {
+    fn neighbours(&self, node_index: usize) -> Vec<usize> {
+        let mut neighbours: Vec<usize> = self
+            .node_targets
+            .iter()
+            .enumerate()
+            .filter(|(_idx, targets)| targets.contains(&node_index))
+            .map(|(idx, _targets)| idx)
+            .collect();
+        neighbours.extend(self.node_targets.get(node_index).unwrap());
+        neighbours
+    }
+}
+
+fn random_location(scale: f64) -> Vec<f64> {
+    let x_loc = js_sys::Math::random() * scale;
+    let y_loc = js_sys::Math::random() * scale;
+    vec![x_loc, y_loc]
+}
+
+#[wasm_bindgen]
+pub struct Rect {
+    bottom_left: Vec<f64>,
+    top_right: Vec<f64>,
+}
+
+#[wasm_bindgen]
+impl Rect {
+    pub fn new(bottom_left: Vec<f64>, top_right: Vec<f64>) -> Rect {
+        Rect {
+            bottom_left,
+            top_right,
+        }
+    }
+}
+
+impl Rect {
+    fn contains(&self, point: &Vec<f64>) -> bool {
+        point[0] > self.bottom_left[0]
+            && point[0] < self.top_right[0]
+            && point[1] > self.bottom_left[1]
+            && point[1] < self.top_right[1]
     }
 }
