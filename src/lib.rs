c0935331c0cc87f4535e066c2e2d89cd03a8dccf
Simon Crowe <simon.r.crowe@protonmail.com>
2022-11-29 17:51:43 +0000

    Optimise node to draw func, make imperative

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 use byteorder::{ByteOrder, LittleEndian};
+use itertools::Itertools;
+
 use js_sys::Reflect::get;
 use js_sys::{Object, Uint8Array};
-use std::collections::HashMap;
-use std::iter::Chain;
-use std::slice::Iter;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{ReadableStreamDefaultReader, Response};
+use web_sys::{window, ReadableStreamDefaultReader, Response};
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
@@ -22,61 +25,96 @@ pub fn get_memory() -> JsValue {
 #[wasm_bindgen]
 pub struct Graph {
     node_targets: Vec<Vec<usize>>,
-    node_sources: HashMap<usize, Vec<usize>>,
+    node_sources: Vec<Vec<usize>>,
     node_locations: Vec<Vec<f64>>,
-    spawn_width: f64,
-    spawn_height: f64,
 }
 
 #[wasm_bindgen]
 impl Graph {
-    pub fn new(display_width: f64, display_height: f64, spawn_scale: f64) -> Graph {
-        let node_targets = Vec::new();
-        let node_sources = HashMap::new();
-        let node_locations = Vec::new();
+    pub fn new(
+        node_count: usize,
+        display_width: f64,
+        display_height: f64,
+        spawn_scale: f64,
+    ) -> Graph {
+        let node_targets = (0..node_count).map(|_| Vec::new()).collect();
+        let node_sources = (0..node_count).map(|_| Vec::new()).collect();
         let spawn_height = display_height * spawn_scale;
         let spawn_width = display_width * spawn_scale;
+        let node_locations = (0..node_count)
+            .map(|_| random_location(spawn_width, spawn_height))
+            .collect();
         Graph {
             node_targets,
             node_sources,
             node_locations,
-            spawn_width,
-            spawn_height,
         }
     }
 
     pub fn node_targets_count(&self, node_id: usize) -> usize {
-        self.node_targets.get(node_id - 1).unwrap().len()
+        self.node_targets.get(node_id).unwrap().len()
     }
 
     pub fn node_targets_ptr(&self, node_id: usize) -> *const usize {
-        self.node_targets.get(node_id - 1).unwrap().as_ptr()
+        self.node_targets.get(node_id).unwrap().as_ptr()
     }
 
     pub fn node_location_ptr(&self, node_id: usize) -> *const f64 {
-        self.node_locations.get(node_id - 1).unwrap().as_ptr()
+        self.node_locations.get(node_id).unwrap().as_ptr()
+    }
+
+    pub fn node_ids_to_render_imp(&self, rect: Rect) -> Vec<usize> {
+        let perf = window().unwrap().performance().unwrap();
+        let start = perf.now();
+
+        let mut indices = rustc_hash::FxHashSet::default();
+
+        for (idx, loc) in self.node_locations.iter().enumerate() {
+            if rect.contains(loc) {
+                indices.insert(idx);
+                for source_idx in self.node_sources.get(idx).unwrap().into_iter() {
+                    indices.insert(*source_idx);
+                }
+                for target_idx in self.node_targets.get(idx).unwrap().into_iter() {
+                    indices.insert(*target_idx);
+                }
+            }
+        }
+        let result = indices.into_iter().collect::<Vec<usize>>();
+
+        let elapsed = perf.now() - start;
+        console_log!("node_ids_to_render took {} ms", elapsed);
+
+        result
     }
 
     pub fn node_ids_to_render(&self, rect: Rect) -> Vec<usize> {
+        let perf = window().unwrap().performance().unwrap();
+        let start = perf.now();
+
         let contained_indices = self
             .node_locations
             .iter()
             .enumerate()
             .filter(|(_idx, loc)| rect.contains(loc))
-            .map(|(idx, _loc)| idx);
+            .map(|(idx, _loc)| idx)
+            .collect::<Vec<usize>>();
 
         let neighbouring_indices = contained_indices
-            .clone()
+            .iter()
             .map(|idx| self.neighbours(idx))
             .flatten();
 
-        let mut result = contained_indices
+        let result = contained_indices
+            .iter()
             .chain(neighbouring_indices)
-            .map(|idx| idx + 1)
-            .collect::<Vec<usize>>();
+            .unique()
+            .map(|idx| *idx)
+            .collect();
 
-        result.sort();
-        result.dedup();
+        let elapsed = perf.now() - start;
+        console_log!("node_ids_to_render took {} ms", elapsed);
+
         result
     }
 
@@ -85,7 +123,7 @@ impl Graph {
         let resp_promise = window.fetch_with_str(&"./targets.bin");
         let resp_value = JsFuture::from(resp_promise).await?;
         let resp: Response = resp_value.dyn_into().unwrap();
-        log(&format!("Response status code: {}", resp.status()));
+        console_log!("Response status code: {}", resp.status());
         if resp.status() != 200 {
             let msg = format!(
                 "Bad status code when getting targets binary: {}",
@@ -96,43 +134,31 @@ impl Graph {
         let reader_value = resp.body().unwrap().get_reader();
         let reader: ReadableStreamDefaultReader = reader_value.dyn_into().unwrap();
 
-        let loc = random_location(self.spawn_width, self.spawn_height);
-        self.node_locations.push(loc);
-        self.node_targets.push(Vec::new());
         let mut current_node_index: usize = 0;
         loop {
             let result_value = JsFuture::from(reader.read()).await?;
             let result: Object = result_value.dyn_into().unwrap();
-            log(&"Got an object from the stream!");
+            console_log!("Got an object from the stream!");
             let done_value = get(&result, &JsValue::from_str("done")).unwrap();
-            log(&"Got a 'done' value from the object!");
+            console_log!("Got a 'done' value from the object!");
             if done_value.as_bool().unwrap() {
-                log(&format!(
-                    "Done. Loaded targets for {} nodes",
-                    current_node_index - 1
-                ));
+                console_log!("Done. Loaded targets for {} nodes", current_node_index - 1);
                 break;
             }
             let chunk_value = get(&result, &JsValue::from_str("value")).unwrap();
-            log(&"Got a value for the stream data!");
+            console_log!("Got a value for the stream data!");
             let chunk_array: Uint8Array = chunk_value.dyn_into().unwrap();
-            log(&"Cast the stream data as an array!");
+            console_log!("Cast the stream data as an array!");
             let chunk_buffer = chunk_array.to_vec();
-            log(&"Converted the stream data to a u8 vector!");
+            console_log!("Converted the stream data to a u8 vector!");
             let mut numbers = vec![0; chunk_buffer.len() / 2];
-            log(&"Instantiated a new u16 vector!");
+            console_log!("Instantiated a new u16 vector!");
             LittleEndian::read_u16_into(&chunk_buffer, &mut numbers);
-            log(&"Filled u16 vector from the buffer!");
-            log(&format!(
-                "Getting targets for node {}...",
-                current_node_index
-            ));
+            console_log!("Filled u16 vector from the buffer!");
+            console_log!("Getting targets for node {}...", current_node_index);
             for &num in numbers.iter() {
                 // The MAX acts as a delimiter
                 if num == u16::MAX {
-                    let loc = random_location(self.spawn_width, self.spawn_height);
-                    self.node_locations.push(loc);
-                    self.node_targets.push(Vec::new());
                     current_node_index += 1;
                 } else {
                     let target_index = num as usize;
@@ -140,12 +166,10 @@ impl Graph {
                         .get_mut(current_node_index)
                         .unwrap()
                         .push(target_index);
-                    if let Some(sources) = self.node_sources.get_mut(&target_index) {
-                        sources.push(current_node_index);
-                    } else {
-                        self.node_sources
-                            .insert(target_index, vec![current_node_index]);
-                    }
+                    self.node_sources
+                        .get_mut(target_index)
+                        .unwrap()
+                        .push(current_node_index);
                 }
             }
         }
@@ -155,10 +179,13 @@ impl Graph {
 }
 
 impl Graph {
-    fn neighbours(&self, node_index: usize) -> Vec<usize> {
-        let mut neighbouring_indices = self.node_sources.get(&node_index).unwrap().to_vec();
-        neighbouring_indices.extend(self.node_targets.get(node_index).unwrap());
-        neighbouring_indices
+    fn neighbours(
+        &self,
+        node_index: &usize,
+    ) -> std::iter::Chain<std::slice::Iter<usize>, std::slice::Iter<usize>> {
+        let sources = self.node_sources.get(*node_index).unwrap().into_iter();
+        let targets = self.node_targets.get(*node_index).unwrap().into_iter();
+        sources.chain(targets)
     }
 }
 
